fc9c2608f8982a6c81688bb5ddd84d67f25d3b1b
Arun Sanna <arunchowdary.sanna@cms.hhs.gov>
2022-04-14 12:43:10 -0700

    updating code

--- elb.tf
+++ elb.tf
@@ -13,15 +13,15 @@ resource "aws_lb" "batcave-lb" {
 
   subnet_mapping {
     subnet_id = var.transport_subnets_by_zone["us-east-1a"]
-    private_ipv4_address = "10.223.166.129"
+    private_ipv4_address = "10.223.166.130"
   }
   subnet_mapping {
     subnet_id = var.transport_subnets_by_zone["us-east-1b"]
-    private_ipv4_address = "10.223.166.145"
+    private_ipv4_address = "10.223.166.146"
   }
   subnet_mapping {
     subnet_id = var.transport_subnets_by_zone["us-east-1c"]
-    private_ipv4_address = "10.223.166.161"
+    private_ipv4_address = "10.223.166.162"
   }
 
   enable_deletion_protection = false
