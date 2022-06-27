d3d044aa398e1bdb196f5f5e935b5e4124e7bf05
Arun Sanna <arunchowdary.sanna@cms.hhs.gov>
2022-06-27 10:25:16 -0700

    updated code for aws auth

--- main.tf
+++ main.tf
@@ -254,6 +254,26 @@ locals {
         ],
       ))
     }
+    {
+      rolearn  = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/${module.eks.self_managed_node_groups.batcave-website.iam_role_name}"
+      username = "system:node:{{EC2PrivateDNSName}}"
+      groups = tolist(concat(
+        [
+          "system:bootstrappers",
+          "system:nodes",
+        ],
+      ))
+    }
+    {
+      rolearn  = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/${module.eks.self_managed_node_groups.batcave-nightlight.iam_role_name}"
+      username = "system:node:{{EC2PrivateDNSName}}"
+      groups = tolist(concat(
+        [
+          "system:bootstrappers",
+          "system:nodes",
+        ],
+      ))
+    }
     # {
     #   rolearn  = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/${module.eks.self_managed_node_groups.memory.iam_role_name}"
     #   username = "system:node:{{EC2PrivateDNSName}}"
