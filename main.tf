2b88e94abb7705745d11595b0fb3f784dc0b1a44
Darcy Cleaver <darcy@leapfrog.ai>
2022-06-30 11:12:56 -0600

    debug oidc provider ref

--- main.tf
+++ main.tf
@@ -297,7 +297,7 @@ resource "aws_security_group_rule" "https-tg-ingress" {
 
 ## Setup for cosign keyless signatures 
 locals {
-  oidc_provider = "oidc.eks.us-east-1.amazonaws.com/id/${aws_iam_openid_connect_provider.oidc_provider.id}"
+  oidc_provider = "oidc.eks.us-east-1.amazonaws.com/id/${module.eks.oidc_provider.id}"
 }
 
 resource "kubernetes_service_account" "cosign" {
