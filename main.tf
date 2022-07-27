d30d273432dcd332a0581d364b9af38a8220adc6
Charles Bushong <charles.bushong@cms.hhs.gov>
2022-07-27 17:17:31 -0400

    BATIAI-719: Fixing kubernetes resource creation

--- main.tf
+++ main.tf
@@ -145,18 +145,13 @@ resource "null_resource" "instance_cleanup" {
 ################################################################################
 # Kubernetes provider configuration
 ################################################################################
-
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
