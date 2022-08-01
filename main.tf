d7c55bac7520935ddad6c1d525f0503835d86f75
justin.lowry <justin@leapfrog.ai>
2022-08-01 10:53:01 -0400

    test

--- main.tf
+++ main.tf
@@ -151,8 +151,8 @@ data "aws_eks_cluster_auth" "cluster" {
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.example.token
 }
 
 resource "aws_kms_key" "eks" {
