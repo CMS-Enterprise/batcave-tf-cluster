d31ce5ea22df0334548c5ce970e256fa5c7c73cb
justin.lowry <justin@leapfrog.ai>
2022-08-01 13:46:55 -0400

    test

--- main.tf
+++ main.tf
@@ -145,6 +145,10 @@ resource "null_resource" "instance_cleanup" {
 ################################################################################
 # Kubernetes provider configuration
 ################################################################################
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
