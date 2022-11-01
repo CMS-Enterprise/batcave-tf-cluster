967fd078d4e785c7c018cdb20c660f6212524c8b
spantacms <113925417+spantacms@users.noreply.github.com>
2022-11-01 16:18:29 -0400

    cni image variable added to EKS cluster CNI add-on BATIAI-1020 (#69)
    * cni image variable added
    
    * addon version section added
    
    * Reviewers suggestion implemented
    
    Co-authored-by: P8TF <saritpanta@ip-10-232-36-147.ec2.internal>

--- variables.tf
+++ variables.tf
@@ -287,6 +287,11 @@ variable "autoscaling_group_tags" {
 
 variable "ami_regex_override" {
   description = "Overrides default AMI lookup regex, which grabs latest AMI matching cluster_version by default"
-  default = ""
-  type = string
+  default     = ""
+  type        = string
+}
+variable "addon_vpc_cni_version" {
+  description = "This is the image of the CNI pod used in the vpc-cni addon.  For other options, run: aws eks describe-addon-versions --add"
+  default     = "v1.11.4-eksbuild.1"
+  string      = string
 }
