695b5def24608d08ba00b28a1329b88590d94abe
Arun Sanna <asanna@vivsoft.io>
2023-06-28 22:03:32 -0400

    updated policies

--- iam-svc.tf
+++ iam-svc.tf
@@ -121,61 +121,61 @@ resource "aws_iam_role_policy_attachment" "appmesh_policy_attachment" {
   policy_arn = aws_iam_policy.appmesh_policy.arn
 }
 
-# data "aws_iam_policy_document" "appmesh_support_policy" {
-#   statement {
-#     effect   = "Allow"
-#     actions  = ["iam:CreateServiceLinkedRole"]
-#     resources = ["arn:aws:iam::*:role/aws-service-role/appmesh.amazonaws.com/AWSServiceRoleForAppMesh"]
-#     condition {
-#       test = "StringLike"
-#       variable = "iam:AWSServiceName"
-#       values = ["appmesh.amazonaws.com"]
-#     }
-#   }
+data "aws_iam_policy_document" "appmesh_support_policy" {
+  statement {
+    effect   = "Allow"
+    actions  = ["iam:CreateServiceLinkedRole"]
+    resources = ["arn:aws:iam::*:role/aws-service-role/appmesh.amazonaws.com/AWSServiceRoleForAppMesh"]
+    condition {
+      test = "StringLike"
+      variable = "iam:AWSServiceName"
+      values = ["appmesh.amazonaws.com"]
+    }
+  }
 
-#   statement {
-#     effect   = "Allow"
-#     actions  = [
-#       "acm:ListCertificates",
-#       "acm:DescribeCertificate",
-#       "acm-pca:DescribeCertificateAuthority",
-#       "acm-pca:ListCertificateAuthorities",
-#     ]
-#     resources = ["*"]
-#   }
+  statement {
+    effect   = "Allow"
+    actions  = [
+      "acm:ListCertificates",
+      "acm:DescribeCertificate",
+      "acm-pca:DescribeCertificateAuthority",
+      "acm-pca:ListCertificateAuthorities",
+    ]
+    resources = ["*"]
+  }
 
-#   statement {
-#     effect   = "Allow"
-#     actions  = [
-#       "servicediscovery:CreateService",
-#       "servicediscovery:DeleteService",
-#       "servicediscovery:GetService",
-#       "servicediscovery:GetInstance",
-#       "servicediscovery:RegisterInstance",
-#       "servicediscovery:DeregisterInstance",
-#       "servicediscovery:ListInstances",
-#       "servicediscovery:ListNamespaces",
-#       "servicediscovery:ListServices",
-#       "servicediscovery:GetInstancesHealthStatus",
-#       "servicediscovery:UpdateInstanceCustomHealthStatus",
-#       "servicediscovery:GetOperation",
-#       "route53:GetHealthCheck",
-#       "route53:CreateHealthCheck",
-#       "route53:UpdateHealthCheck",
-#       "route53:ChangeResourceRecordSets",
-#       "route53:DeleteHealthCheck",
-#     ]
-#     resources = ["*"]
-#   }
-# }
+  statement {
+    effect   = "Allow"
+    actions  = [
+      "servicediscovery:CreateService",
+      "servicediscovery:DeleteService",
+      "servicediscovery:GetService",
+      "servicediscovery:GetInstance",
+      "servicediscovery:RegisterInstance",
+      "servicediscovery:DeregisterInstance",
+      "servicediscovery:ListInstances",
+      "servicediscovery:ListNamespaces",
+      "servicediscovery:ListServices",
+      "servicediscovery:GetInstancesHealthStatus",
+      "servicediscovery:UpdateInstanceCustomHealthStatus",
+      "servicediscovery:GetOperation",
+      "route53:GetHealthCheck",
+      "route53:CreateHealthCheck",
+      "route53:UpdateHealthCheck",
+      "route53:ChangeResourceRecordSets",
+      "route53:DeleteHealthCheck",
+    ]
+    resources = ["*"]
+  }
+}
 
-# resource "aws_iam_policy" "appmesh_support_policy" {
-#   name   = "appmesh_support_policy"
-#   policy = data.aws_iam_policy_document.appmesh_support_policy.json
-# }
+resource "aws_iam_policy" "appmesh_support_policy" {
+  name   = "appmesh_support_policy"
+  path        = var.iam_role_path
+  policy = data.aws_iam_policy_document.appmesh_support_policy.json
+}
 
-
-# resource "aws_iam_role_policy_attachment" "appmesh_support_policy_attachment" {
-#  role = aws_iam_role.appmesh_role.name
-#  policy_arn = aws_iam_policy.appmesh_support_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "appmesh_support_policy_attachment" {
+ role = aws_iam_role.appmesh_role.name
+ policy_arn = aws_iam_policy.appmesh_support_policy.arn
+}
