273998e4850d625b0914a5e1d091f75634dabefd
Arun Sanna <asanna@vivsoft.io>
2023-06-28 20:47:26 -0400

    added role

--- iam-svc.tf
+++ iam-svc.tf
@@ -137,7 +137,7 @@ resource "aws_iam_role" "appmesh_role" {
   path        = var.role_path
   description = var.role_description
 
-  assume_role_policy    = data.aws_iam_policy_document.appmesh_policy.data
+  assume_role_policy    = data.aws_iam_policy_document.appmesh_policy.json
   max_session_duration  = var.max_session_duration
   permissions_boundary  = var.role_permissions_boundary_arn
   force_detach_policies = var.force_detach_policies
