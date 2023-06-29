b0a63cb33ab20441f44776b1d3149baba5205805
Arun Sanna <asanna@vivsoft.io>
2023-06-28 21:58:32 -0400

    updated policies

--- iam-svc.tf
+++ iam-svc.tf
@@ -66,49 +66,54 @@ resource "aws_iam_role" "appmesh_role" {
   tags = var.tags
 }
 
-# data "aws_iam_policy_document" "appmesh_policy" {
-#   statement {
-#     effect  = "Allow"
-#     actions = [
-#       "appmesh:ListVirtualRouters",
-#       "appmesh:ListVirtualServices",
-#       "appmesh:ListRoutes",
-#       "appmesh:ListGatewayRoutes",
-#       "appmesh:ListMeshes",
-#       "appmesh:ListVirtualNodes",
-#       "appmesh:ListVirtualGateways",
-#       "appmesh:DescribeMesh",
-#       "appmesh:DescribeVirtualRouter",
-#       "appmesh:DescribeRoute",
-#       "appmesh:DescribeVirtualNode",
-#       "appmesh:DescribeVirtualGateway",
-#       "appmesh:DescribeGatewayRoute",
-#       "appmesh:DescribeVirtualService",
-#       "appmesh:CreateMesh",
-#       "appmesh:CreateVirtualRouter",
-#       "appmesh:CreateVirtualGateway",
-#       "appmesh:CreateVirtualService",
-#       "appmesh:CreateGatewayRoute",
-#       "appmesh:CreateRoute",
-#       "appmesh:CreateVirtualNode",
-#       "appmesh:UpdateMesh",
-#       "appmesh:UpdateRoute",
-#       "appmesh:UpdateVirtualGateway",
-#       "appmesh:UpdateVirtualRouter",
-#       "appmesh:UpdateGatewayRoute",
-#       "appmesh:UpdateVirtualService",
-#       "appmesh:UpdateVirtualNode",
-#       "appmesh:DeleteMesh",
-#       "appmesh:DeleteRoute",
-#       "appmesh:DeleteVirtualRouter",
-#       "appmesh:DeleteGatewayRoute",
-#       "appmesh:DeleteVirtualService",
-#       "appmesh:DeleteVirtualNode",
-#       "appmesh:DeleteVirtualGateway",
-#     ]
-#     resources = ["*"]
-#   }
-# }
+data "aws_iam_policy_document" "appmesh_policy" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "appmesh:ListVirtualRouters",
+      "appmesh:ListVirtualServices",
+      "appmesh:ListRoutes",
+      "appmesh:ListGatewayRoutes",
+      "appmesh:ListMeshes",
+      "appmesh:ListVirtualNodes",
+      "appmesh:ListVirtualGateways",
+      "appmesh:DescribeMesh",
+      "appmesh:DescribeVirtualRouter",
+      "appmesh:DescribeRoute",
+      "appmesh:DescribeVirtualNode",
+      "appmesh:DescribeVirtualGateway",
+      "appmesh:DescribeGatewayRoute",
+      "appmesh:DescribeVirtualService",
+      "appmesh:CreateMesh",
+      "appmesh:CreateVirtualRouter",
+      "appmesh:CreateVirtualGateway",
+      "appmesh:CreateVirtualService",
+      "appmesh:CreateGatewayRoute",
+      "appmesh:CreateRoute",
+      "appmesh:CreateVirtualNode",
+      "appmesh:UpdateMesh",
+      "appmesh:UpdateRoute",
+      "appmesh:UpdateVirtualGateway",
+      "appmesh:UpdateVirtualRouter",
+      "appmesh:UpdateGatewayRoute",
+      "appmesh:UpdateVirtualService",
+      "appmesh:UpdateVirtualNode",
+      "appmesh:DeleteMesh",
+      "appmesh:DeleteRoute",
+      "appmesh:DeleteVirtualRouter",
+      "appmesh:DeleteGatewayRoute",
+      "appmesh:DeleteVirtualService",
+      "appmesh:DeleteVirtualNode",
+      "appmesh:DeleteVirtualGateway",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "appmesh_policy" {
+  name   = "appmesh_policy"
+  policy = data.aws_iam_policy_document.appmesh_policy.json
+}
 
 # data "aws_iam_policy_document" "appmesh_support_policy" {
 #   statement {
@@ -161,10 +166,6 @@ resource "aws_iam_role" "appmesh_role" {
 
 
 
-# resource "aws_iam_policy" "appmesh_policy" {
-#   name   = "appmesh_policy"
-#   policy = data.aws_iam_policy_document.appmesh_policy.json
-# }
 
 
 # resource "aws_iam_policy" "appmesh_support_policy" {
