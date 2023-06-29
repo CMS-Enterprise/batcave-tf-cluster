2191fe09521929495e1eab3dd36d3c8fdef89dd0
Arun Sanna <asanna@vivsoft.io>
2023-06-28 20:43:37 -0400

    added role

--- iam-svc.tf
+++ iam-svc.tf
@@ -41,102 +41,96 @@
 #   role       = aws_iam_role.this[0].name
 # }
 
-
-resource "aws_iam_policy" "appmesh_policy" {
-  name        = "AppMesh-IAM"
-  description = "IAM policy for AppMesh"
-  path        = var.iam_role_path
-
-  policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = [
-          "appmesh:ListVirtualRouters",
-          "appmesh:ListVirtualServices",
-          "appmesh:ListRoutes",
-          "appmesh:ListGatewayRoutes",
-          "appmesh:ListMeshes",
-          "appmesh:ListVirtualNodes",
-          "appmesh:ListVirtualGateways",
-          "appmesh:DescribeMesh",
-          "appmesh:DescribeVirtualRouter",
-          "appmesh:DescribeRoute",
-          "appmesh:DescribeVirtualNode",
-          "appmesh:DescribeVirtualGateway",
-          "appmesh:DescribeGatewayRoute",
-          "appmesh:DescribeVirtualService",
-          "appmesh:CreateMesh",
-          "appmesh:CreateVirtualRouter",
-          "appmesh:CreateVirtualGateway",
-          "appmesh:CreateVirtualService",
-          "appmesh:CreateGatewayRoute",
-          "appmesh:CreateRoute",
-          "appmesh:CreateVirtualNode",
-          "appmesh:UpdateMesh",
-          "appmesh:UpdateRoute",
-          "appmesh:UpdateVirtualGateway",
-          "appmesh:UpdateVirtualRouter",
-          "appmesh:UpdateGatewayRoute",
-          "appmesh:UpdateVirtualService",
-          "appmesh:UpdateVirtualNode",
-          "appmesh:DeleteMesh",
-          "appmesh:DeleteRoute",
-          "appmesh:DeleteVirtualRouter",
-          "appmesh:DeleteGatewayRoute",
-          "appmesh:DeleteVirtualService",
-          "appmesh:DeleteVirtualNode",
-          "appmesh:DeleteVirtualGateway"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect    = "Allow"
-        Action    = ["iam:CreateServiceLinkedRole"]
-        Resource  = "arn:aws:iam::*:role/aws-service-role/appmesh.amazonaws.com/AWSServiceRoleForAppMesh"
-        Condition = {
-          StringLike: {
-            "iam:AWSServiceName" : ["appmesh.amazonaws.com"]
-          }
-        }
-      },
-      {
-        Effect   = "Allow"
-        Action   = [
-          "acm:ListCertificates",
-          "acm:DescribeCertificate",
-          "acm-pca:DescribeCertificateAuthority",
-          "acm-pca:ListCertificateAuthorities"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect   = "Allow"
-        Action   = [
-          "servicediscovery:CreateService",
-          "servicediscovery:DeleteService",
-          "servicediscovery:GetService",
-          "servicediscovery:GetInstance",
-          "servicediscovery:RegisterInstance",
-          "servicediscovery:DeregisterInstance",
-          "servicediscovery:ListInstances",
-          "servicediscovery:ListNamespaces",
-          "servicediscovery:ListServices",
-          "servicediscovery:GetInstancesHealthStatus",
-          "servicediscovery:UpdateInstanceCustomHealthStatus",
-          "servicediscovery:GetOperation",
-          "route53:GetHealthCheck",
-          "route53:CreateHealthCheck",
-          "route53:UpdateHealthCheck",
-          "route53:ChangeResourceRecordSets",
-          "route53:DeleteHealthCheck"
-        ]
-        Resource = "*"
-      }
+data "aws_iam_policy_document" "appmesh_policy" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "appmesh:ListVirtualRouters",
+      "appmesh:ListVirtualServices",
+      "appmesh:ListRoutes",
+      "appmesh:ListGatewayRoutes",
+      "appmesh:ListMeshes",
+      "appmesh:ListVirtualNodes",
+      "appmesh:ListVirtualGateways",
+      "appmesh:DescribeMesh",
+      "appmesh:DescribeVirtualRouter",
+      "appmesh:DescribeRoute",
+      "appmesh:DescribeVirtualNode",
+      "appmesh:DescribeVirtualGateway",
+      "appmesh:DescribeGatewayRoute",
+      "appmesh:DescribeVirtualService",
+      "appmesh:CreateMesh",
+      "appmesh:CreateVirtualRouter",
+      "appmesh:CreateVirtualGateway",
+      "appmesh:CreateVirtualService",
+      "appmesh:CreateGatewayRoute",
+      "appmesh:CreateRoute",
+      "appmesh:CreateVirtualNode",
+      "appmesh:UpdateMesh",
+      "appmesh:UpdateRoute",
+      "appmesh:UpdateVirtualGateway",
+      "appmesh:UpdateVirtualRouter",
+      "appmesh:UpdateGatewayRoute",
+      "appmesh:UpdateVirtualService",
+      "appmesh:UpdateVirtualNode",
+      "appmesh:DeleteMesh",
+      "appmesh:DeleteRoute",
+      "appmesh:DeleteVirtualRouter",
+      "appmesh:DeleteGatewayRoute",
+      "appmesh:DeleteVirtualService",
+      "appmesh:DeleteVirtualNode",
+      "appmesh:DeleteVirtualGateway",
     ]
-  })
+    resources = ["*"]
+  }
+
+  statement {
+    effect   = "Allow"
+    actions  = ["iam:CreateServiceLinkedRole"]
+    resources = ["arn:aws:iam::*:role/aws-service-role/appmesh.amazonaws.com/AWSServiceRoleForAppMesh"]
+    condition {
+      test = "StringLike"
+      variable = "iam:AWSServiceName"
+      values = ["appmesh.amazonaws.com"]
+    }
+  }
+
+  statement {
+    effect   = "Allow"
+    actions  = [
+      "acm:ListCertificates",
+      "acm:DescribeCertificate",
+      "acm-pca:DescribeCertificateAuthority",
+      "acm-pca:ListCertificateAuthorities",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect   = "Allow"
+    actions  = [
+      "servicediscovery:CreateService",
+      "servicediscovery:DeleteService",
+      "servicediscovery:GetService",
+      "servicediscovery:GetInstance",
+      "servicediscovery:RegisterInstance",
+      "servicediscovery:DeregisterInstance",
+      "servicediscovery:ListInstances",
+      "servicediscovery:ListNamespaces",
+      "servicediscovery:ListServices",
+      "servicediscovery:GetInstancesHealthStatus",
+      "servicediscovery:UpdateInstanceCustomHealthStatus",
+      "servicediscovery:GetOperation",
+      "route53:GetHealthCheck",
+      "route53:CreateHealthCheck",
+      "route53:UpdateHealthCheck",
+      "route53:ChangeResourceRecordSets",
+      "route53:DeleteHealthCheck",
+    ]
+    resources = ["*"]
+  }
 }
+
 
 resource "aws_iam_role" "appmesh_role" {
   name               = "appmesh-role"
