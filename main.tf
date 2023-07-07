14ee9c308854f2e957452361d6fa4f0af2f1b743
hpalaparthi <hpalaparthi@vivsoft.io>
2023-07-07 09:27:29 -0400

    removed aws auth kube resource and added directly to the module

--- main.tf
+++ main.tf
@@ -166,9 +166,29 @@ module "eks" {
   enable_irsa                             = true
 
   # Self managed node groups will not automatically create the aws-auth configmap so we need to
-  create_aws_auth_configmap = true
+  #create_aws_auth_configmap = true
+  #manage_aws_auth_configmap = true
+
+  # aws-auth configmap
   manage_aws_auth_configmap = true
 
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/ct-ado-batcave-developer-admin",
+      username = "ct-ado-batcave-developer-admin",
+      groups   = ["system:masters"]
+    },
+    {
+      rolearn  = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/batcave-github-actions-role",
+      username = "batcave-github-actions-role",
+      groups   = ["system:masters"]
+    },
+    {
+      rolearn  = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/aolytix-role",
+      username = "aolytix-role",
+      groups   = ["system:masters"]
+    }
+  ]
   # This is handled externally
   create_kms_key = false
 
