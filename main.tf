6f7fe8579fa372fe1158c63ececd19f2f4691403
Arun Sanna <asanna@vivsoft.io>
2023-07-20 12:35:11 -0400

    Added iam permissions boundary

--- main.tf
+++ main.tf
@@ -190,7 +190,7 @@ module "eks" {
     subnet_ids = coalescelist(var.host_subnets, var.private_subnets)
     attach_cluster_primary_security_group = true
     iam_role_permissions_boundary = var.iam_role_permissions_boundary
-    iam_role_path - var.iam_role_path
+    iam_role_path = var.iam_role_path
   }
 
   eks_managed_node_groups = {
