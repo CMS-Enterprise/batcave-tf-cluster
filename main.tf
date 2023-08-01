eae7c0e6d9b851054b0971a8bda66a04eecf6db7
Arun Sanna <asanna@vivsoft.io>
2023-08-01 11:14:08 -0400

    testing rotation with labels

--- main.tf
+++ main.tf
@@ -208,6 +208,8 @@ module "eks" {
       iam_role_path = var.iam_role_path
       
       pre_bootstrap_user_data = "sysctl -w net.ipv4.ip_forward=1\n"
+      bootstrap_extra_args = "--kubelet-extra-args '--node-labels=general=true'"
+
       instance_types = ["c5.4xlarge"]
       labels = {
         Environment = "test"
