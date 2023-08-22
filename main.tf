a61ac4643caa46cc4e2141ff70e1a0882c3ead4d
Arun Sanna <asanna@vivsoft.io>
2023-08-21 21:57:33 -0400

    testing by passing same locals to the eks managed node groups

--- main.tf
+++ main.tf
@@ -186,51 +186,10 @@ module "eks" {
 
 # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    instance_types = ["c5.xlarge"]
     subnet_ids = coalescelist(var.host_subnets, var.private_subnets)
-    attach_cluster_primary_security_group = true
-    iam_role_permissions_boundary = var.iam_role_permissions_boundary
-    iam_role_path = var.iam_role_path
-    
   }
 
-  eks_managed_node_groups = {
-    green = {
-      ami_type = "AL2_x86_64"
-      ami_id  = data.aws_ami.eks_ami.id
-      platform = "linux"
-      enable_bootstrap_user_data = true
-      
-      min_size     = 1
-      max_size     = 5
-      desired_size = 1
-      iam_role_permissions_boundary = var.iam_role_permissions_boundary
-      iam_role_path = var.iam_role_path
-      
-      pre_bootstrap_user_data = "sysctl -w net.ipv4.ip_forward=1\n"
-      instance_types = ["c5.4xlarge"]
-      labels = {
-        genera1 = "true"
-        type    = "eks_managed"
-      }
-
-      taints = {
-        dedicated = {
-          key    = "bat_app"
-          value  = "utility_belt"
-          effect = "NO_SCHEDULE"
-        }
-      }
-
-      update_config = {
-        max_unavailable_percentage = 33 # or set `max_unavailable`
-      }
-
-      tags = {
-        Name = "srii-test"
-      }
-    }
-  }
+  eks_managed_node_groups = local.custom_node_pools
 # CLUSTER Addons
   cluster_addons = {
     #vpc-cni = {
