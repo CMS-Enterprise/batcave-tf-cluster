6a24849c8b4b8bf40a38b30702ef527539159a5f
Arun Sanna <asanna@vivsoft.io>
2023-11-02 12:57:52 -0400

    updated code with foreach

--- outputs.tf
+++ outputs.tf
@@ -184,3 +184,8 @@ output "batcave_alb_shared_dns" {
   description = "DNS value of ALB created for proxying requests through an ALB in the shared subnet"
   value       = var.create_alb_shared ? aws_lb.batcave_alb_shared[0].dns_name : ""
 }
+
+
+output "local-test" {
+  value = local.custom_node_pools
+}
