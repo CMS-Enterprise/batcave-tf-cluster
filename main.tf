6007e38cf6c9131f062d617ec97a72bd6cb71f24
Arun Sanna <asanna@vivsoft.io>
2023-11-02 01:02:03 -0400

    updated dependecies

--- main.tf
+++ main.tf
@@ -513,10 +513,10 @@ locals{
 # }
 
 resource "aws_autoscaling_attachment" "eks_managed_node_groups_alb_attachment" {
- for_each = local.autoscaling_groups
- autoscaling_group_name = each.value
- lb_target_group_arn = aws_lb_target_group.batcave_alb_https.arn
- depends_on = [  
+  for_each = length(local.autoscaling_groups) > 0 ? toset(local.autoscaling_groups) : toset([])
+  autoscaling_group_name = each.value
+  lb_target_group_arn = aws_lb_target_group.batcave_alb_https.arn
+  depends_on = [  
     module.eks_managed_node_groups
   ]
 }
