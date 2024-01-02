cc34a45659d84d790e7d568d831bc32fddc061aa
James Elliott <jelliott@jelliott02.local>
2024-01-02 11:36:48 -0500

    incorporating Charles' feedback

--- alb-shared-to-k8s.tf
+++ alb-shared-to-k8s.tf
@@ -176,12 +176,7 @@ resource "aws_security_group_rule" "batcave_alb_shared_egress" {
   security_group_id = aws_security_group.batcave_alb_shared[0].id
 }
 
-data "aws_wafv2_web_acl" "cms_waf" {
-  name  = "RegSamQuickACLEnforcingV2"
-  scope = "REGIONAL"
-}
-
-resource "aws_wafv2_web_acl_association" "cms_waf_assoc" {
+resource "aws_wafv2_web_acl_association" "batcave_alb_shared_cms_waf_assoc" {
   count        = var.create_alb_shared ? 1 : 0
   resource_arn = aws_lb.batcave_alb_shared[0].arn
   web_acl_arn  = data.aws_wafv2_web_acl.cms_waf.arn
