863b783c962882ad6a9ecbe82845f66aaba88203
dahoward5 <derek.howard@intellibridge.us>
2024-03-15 11:03:34 -0400

    remove unneeded var reference

--- main.tf
+++ main.tf
@@ -464,7 +464,7 @@ resource "aws_iam_role" "cosign" {
 #}
 
 resource "aws_autoscaling_attachment" "eks_managed_node_groups_alb_attachment" {
-  for_each               = var.enable_eks_managed_nodes ? { for np in local.eks_node_pools : np.name => np } : {}
+  for_each               = { for np in local.eks_node_pools : np.name => np }
   autoscaling_group_name = try(module.eks_managed_node_groups[each.value.group_name].node_group_autoscaling_group_names[0], "")
 
   lb_target_group_arn = aws_lb_target_group.batcave_alb_https.arn
@@ -475,7 +475,7 @@ resource "aws_autoscaling_attachment" "eks_managed_node_groups_alb_attachment" {
 }
 
 resource "aws_autoscaling_attachment" "eks_managed_node_groups_proxy_attachment" {
-  for_each               = var.create_alb_proxy && var.enable_eks_managed_nodes ? { for np in local.eks_node_pools : np.name => np } : {}
+  for_each               = var.create_alb_proxy ? { for np in local.eks_node_pools : np.name => np } : {}
   autoscaling_group_name = try(module.eks_managed_node_groups[each.value.group_name].node_group_autoscaling_group_names[0], "")
 
   lb_target_group_arn = var.create_alb_proxy ? aws_lb_target_group.batcave_alb_proxy_https[0].arn : null
@@ -486,7 +486,7 @@ resource "aws_autoscaling_attachment" "eks_managed_node_groups_proxy_attachment"
 }
 
 resource "aws_autoscaling_attachment" "eks_managed_node_groups_shared_attachment" {
-  for_each               = var.create_alb_shared && var.enable_eks_managed_nodes ? { for np in local.eks_node_pools : np.name => np } : {}
+  for_each               = var.create_alb_shared ? { for np in local.eks_node_pools : np.name => np } : {}
   autoscaling_group_name = try(module.eks_managed_node_groups[each.value.group_name].node_group_autoscaling_group_names[0], "")
 
   lb_target_group_arn = var.create_alb_shared ? aws_lb_target_group.batcave_alb_shared_https[0].arn : null
