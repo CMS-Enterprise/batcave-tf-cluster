243308c3dd05b7016d46614d2672933595422815
Arun Sanna <asanna@vivsoft.io>
2024-04-23 23:07:22 -0400

    UPDATED: Managed AWS auth configmap is set to false
    - The 'manage_aws_auth_configmap' variable in module is now set to false.

--- main.tf
+++ main.tf
@@ -252,6 +252,7 @@ module "eks" {
   enable_cluster_creator_admin_permissions = true
   vpc_id                                   = var.vpc_id
   subnet_ids                               = var.private_subnets
+  manage_aws_auth_configmap                = false
 
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = false
